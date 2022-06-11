f172de8f0f22a5e4573fc965cc78cc05bdac9ebb
Buğra Can Ünsal <onsal9193@gmail.com>
2022-06-11 16:07:54 +0300

    SayiSiralama.java is modified

--- SayiSiralama.java
+++ SayiSiralama.java
@@ -14,26 +14,26 @@ public class SayiSiralama {
         if (a == b && b == c) {
             System.out.println(a + " = " + b + " = " + c);
         } else if (a == b && b > c ) {
-            System.out.println(a + " = " + b + " > " + c);
+            System.out.println(c+ " <" + a + " = " + b);
         } else if (a == b && c > b) {
-            System.out.println(c + " > " + b + " = " + a);
+            System.out.println(b + " = " + a + " < " + c);
         } else if (b == c && b > a) {
-            System.out.println(b + " = " + c + " > " + a);
+            System.out.println(a + " < " + c + " = " + b);
         } else if (b == c && a > b) {
-            System.out.println(a + " > " + b + " = " + c);
+            System.out.println(c + " = " + b + " < " + a);
         } else if (a == c && a > b) {
-            System.out.println(a + " = " + c + " > " + b);
+            System.out.println(b + " < " + c + " = " + a);
         } else if (a == c && b > a) {
-            System.out.println(b + " > " + a + " = "+ c);
+            System.out.println(c + " = " + a + " < "+ b);
         } else if (a > b && a > c ){
-            if (b > c) System.out.println(a + " > " + b + " > " + c);
-            else System.out.println(a + " > " + c + " > " + b);
+            if (b > c) System.out.println(c + " < " + b + " < " + a);
+            else System.out.println(b + " > " + c + " < " + a);
         } else if (b > c && b > a) {
-            if (a > c) System.out.println(b + " > " + a + " > " + c);
-            else System.out.println(b + " > " + c + " > " + a);
+            if (a > c) System.out.println(c + " < " + a + " < " + b);
+            else System.out.println(a + " < " + c + " < " + a);
         } else if (c > b && c > a) {
-            if (b > a) System.out.println(c + " > " + b + " > " + a);
-            else System.out.println(c + " > " + a + " > " + b);
+            if (b > a) System.out.println(a + " < " + b + " < " + c);
+            else System.out.println(b + " < " + a + " < " + c);
         }
     }
 }
