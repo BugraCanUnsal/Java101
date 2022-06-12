8b555ee0d47c48c75c8d4cb19a2f6d6e52814ad7
Buğra Can Ünsal <onsal9193@gmail.com>
2022-06-12 15:48:31 +0300

    MukemmelSayi.java is added

---
+++ MukemmelSayi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayiyi giriniz : ");
+        int number = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number%i == 0){
+                sum+=i;
+            }
+        }
+
+        if (sum == number){
+            System.out.println(number + " mukemmel sayidir.");
+        }else System.out.println(number + " mukemmel sayi degildir.");
+    }
+}
